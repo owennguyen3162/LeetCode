3506212585e8b41b933c9a15f7722111715a0468
owennguyen3162 <nguyentuan3162@gmail.com>
2024-06-10 23:33:14 +0700

    Two sum

---
+++ LeetCodeSolution/src/Main.java
@@ -0,0 +1,14 @@
+import TwoSum.Solution;
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Solution twoSum = new Solution();
+
+        System.out.println(Arrays.toString(twoSum.Solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 7)));
+    }
+}
